bb5f199ce3dd1e22ee89d8281462b5d6eeedf683
aquoss <amberquoss@gmail.com>
2017-04-07 14:11:03 -0700

    uses add_secure_token gem for auth_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407202246) do
+ActiveRecord::Schema.define(version: 20170407210817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20170407202246) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.integer  "shopping_cart_id"
+    t.string   "token"
   end
 
   add_foreign_key "cart_products", "products"
