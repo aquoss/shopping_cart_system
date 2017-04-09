5744608cf355d706125390686f8daa11e9062828
aquoss <amberquoss@gmail.com>
2017-04-09 11:24:36 -0700

    creates routes for adding and removing products from cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  delete '/:shopping_cart_id/products/:cart_product_id', to: 'cart_products#destroy'
+
+  post '/shopping_carts/:shopping_cart_id/cart_products/:cart_product_id', to: 'cart_products#create'
+  delete '/shopping_carts/:shopping_cart_id/cart_products/:cart_product_id', to: 'cart_products#destroy'
 
   get '/order_history', to: 'orders#index', as: 'order_history_path'
 
