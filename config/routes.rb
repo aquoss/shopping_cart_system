04b57f64087ec76312576f591bc918b842996f82
aquoss <amberquoss@gmail.com>
2017-04-08 21:54:00 -0700

    matches routes to tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  delete '/:shopping_cart_id/products/:cart_product_id', to: 'cart_products#destroy'
+
+  get '/order_history', to: 'orders#index', as: 'order_history_path'
+
 end
