e30dad90f4da533a2bdd62a3bf9cd247da135c52
aquoss <amberquoss@gmail.com>
2017-04-09 23:37:37 -0700

    creates seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,17 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(first_name: "John", last_name: "Doe", email: "john@gmail.com")
+shopping_cart = ShoppingCart.create(user_id: user.id, number_of_items: 0, total_price: 0.00)
+
+style1 = Style.create(title:"The Cotton Crew", department: "Apparel", style_class: "Tops", business_group: "Womens", description: "An every-day tee with a rounded neck")
+product1 = Product.create(style_id: style1.id, color: "red", material: "cotton", print: "heathered", price: 25.00)
+Inventory.create(product_id: product1.id, size: "S", available_inventory: 30)
+Inventory.create(product_id: product1.id, size: "M", available_inventory: 20)
+Inventory.create(product_id: product1.id, size: "L", available_inventory: 1)
+
+style2 = Style.create(title:"The Sport Dress", department: "Apparel", style_class: "Dresses", business_group: "Womens", description: "A sleek and comfy dress")
+product2 = Product.create(style_id: style2.id, color: "blue", material: "polyester", print: "solid", price: 30.00)
+Inventory.create(product_id: product2.id, size: "S", available_inventory: 15)
+Inventory.create(product_id: product2.id, size: "M", available_inventory: 5)
+Inventory.create(product_id: product2.id, size: "L", available_inventory: 10)
